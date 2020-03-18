a4dac8cae17e635bcedf22b36166cb2fb6c11fb7
omar-dahdouh <omar.m.dahdouh@gmail.com>
2020-03-18 11:19:01 +0200

    basic layout relates #3

---
+++ src/components/orgs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Orgs extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return <div className="orgs">GSG-G8</div>;
+  }
+}
